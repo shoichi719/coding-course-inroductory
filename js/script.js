1a9b4be308b23901ad973f2d667c13bf0341a9b1
shoichi nagasaki <clark.zoro@gmail.com>
2025-11-04 22:29:45 +0900

    hamburger-menu responsive 追記

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-$(function () {});
+$(function () {
+  // hamburger-menu
+  $(".js-hamburger, .js-drawer, .js-drawer a").click(function () {
+    // ↑の三つのクラスのいずれかがクリックされた時に、
+    $(".js-hamburger").toggleClass("is-active");
+    // js-hamburgerが開閉された時にis-activeクラスを付与しますよ。
+    $(".js-drawer").fadeToggle();
+    // js-drawerはフェードイン、フェードアウトします。
+  });
+});
+
+
+
